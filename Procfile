def2ab1b09fb3557809e8b255c56f818305f0905
Ney Marlon <ney.marlon@hotmail.com>
2022-08-23 09:06:05 -0300

    deploy incial

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gnicorn meusite:app
---
+++ meusite.exe.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+
+@app.route("/contatos")
+def contatos():
+    return render_template("contatos.html")
+
+
+@app.route("/usuarios/<nome_usuarios>")
+def usuarios(nome_usuarios):
+    return render_template("usuarios.html", nome_usuarios=nome_usuarios)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
